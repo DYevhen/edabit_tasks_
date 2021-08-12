16f76b479f74f66c0ab6a7dcfd86a73013a9f37a
ydobrenchuk <ydobrenchuk@exadel.com>
2021-08-12 13:55:19 +0300

    task1

---
+++ src/task1.java
@@ -0,0 +1,17 @@
+public class task1 {
+    /**
+     Difficult: Easy
+     Instruction: Create a function which returns the number of true values there are in an array.
+     Example: countTrue([true, false, false, true, false]) ➞ 2
+     */
+    public static int countTrue(boolean[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
